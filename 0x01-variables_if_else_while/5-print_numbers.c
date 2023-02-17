c806938be673498a428f8e9483dfeb19a4e4bddd
Raji Gbenga Kabir <rajigbenga5@gmail.com>
2023-02-17 09:46:20 -0800

    Numbers

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/**
+ * main - a program that prints all single base 10 numbers
+ *
+ * Return: 0(Success)
+ */
+
+int main(void)
+{
+	int i;
+
+	for (i = 0; i < 10; i++)
+		printf("%d", i);
+	printf("\n");
+
+	return (0);
+}
