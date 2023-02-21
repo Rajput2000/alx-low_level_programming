2c62a1a120e72e8332b91be9b79f5886124e86be
Raji Gbenga Kabir <rajigbenga5@gmail.com>
2023-02-21 09:55:04 -0800

    adding files

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -10,8 +10,6 @@ int main(void)
 {       
 	int count;
 	unsigned long fib1 = 0, fib2 = 1,sum;
-	unsigned long fib1_half1, fib1_half2, fib2_half1, fib2_half2;
-	unsigned long half1, half2;
         
         for (count = 0; count<85; count++)
         {       
@@ -20,11 +18,6 @@ int main(void)
                 fib1 = fib2;
                 fib2 = sum;
         }       
-        fib1_half1 = fib1 / 10000000000;
-        fib2_half1 = fib2 / 10000000000;
-        fib1_half2 = fib1 % 10000000000;
-        fib2_half2 = fib2 % 10000000000;
-        
         printf("\n");
         return (0);
 }
