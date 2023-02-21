0392d830a3c998d1684871a302ee759e900df21c
Raji Gbenga Kabir <rajigbenga5@gmail.com>
2023-02-21 08:29:22 -0800

    Nature made the natural numbers; All else is the work of women

---
+++ 0x02-functions_nested_loops/101-natural.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ * main - a program that sums all multiples of 3 and 5
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	int i, sum = 0;
+
+	for (i = 0; i < 1024; i++)
+	{
+		if ((i % 5) == 0 || (i % 3) == 0)
+			sum += i;
+	}
+	printf("%d\n", sum);
+
+	return (0);
+}
