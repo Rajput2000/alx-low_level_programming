c3b1e25fc59cb69bfca6fd37a84c13f7bdd555c6
Raji Gbenga Kabir <rajigbenga5@gmail.com>
2023-02-23 05:10:38 -0800

    The problem of distinguishing prime numbers from composite numbers and of resolving the latter into their prime factors is known to be one of the most important and useful in arithmetic

--- 0x04-more_functions_nested_loops/100-prime_factor.c
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -1,83 +1,45 @@
+/**
+ * C program to find all prime factors of a given number
+ */
+
 #include <stdio.h>
-#include "main.h"
-/**
- * main - prints the biggest prime factor of a number.
- *
- * Return: Always 0.
- */
-int main(void)
+
+int main()
 {
-	long int number;
+    int i, j, num, isPrime, max;
+    
+    max = 0;
+    num = 612852475143;
 
-	number = 612852475143;
+    /* Find all Prime factors */
+    for(i=2; i<=num; i++)
+    {
+        /* Check 'i' for factor of num */
+        if(num%i==0)
+        {
+            /* Check 'i' for Prime */
+            isPrime = 1;
+            for(j=2; j<=i/2; j++)
+            {
+                if(i%j==0)
+                {
+                    isPrime = 0;
+                    break;
+                }
+            }
 
-	if (isPrime(number) == 1)
-	{
-		printf("%ld\n", number);
-	}
-	else
-	{
-		printf("%ld\n", biggestFactor(number));
-	}
-	return (0);
-}
-/**
- * isPrime - analise if a number is prime or not
- * @n: number to check
- * Return: true if it is prime false if not
- */
-int isPrime(long int n)
-{
-	int i;
+            /* If 'i' is Prime number and factor of num */
+            if(isPrime==1)
+            {
+                if (i > max)
+                {
+                    max = i;
+                }
+            }
+        }
+    }
 
-	if (n <= 1)
-	{
-		return (0);
-	}
-	else if (n == 2)
-	{
-		return (1);
-	}
-	else
-	{
-		for (i = 2; i < n; i++)
-		{
-			if (n % i == 0)
-			{
-				return (0);
-			}
-		}
-	return (1);
-	}
-}
-/**
- * biggestFactor - returns the biggest prime factor of a number
- * @a: number to check
- * Return: biggest factor
- */
-long int biggestFactor(long int a)
-{
-	long int i, factor;
+    printf("%d", max);
 
-	factor = a;
-	for (i = 2; i <= factor; i++)
-	{
-		if (isPrime(factor) == 1)
-		{
-			break;
-		}
-		else
-		{
-			if ((factor % i == 0) && (isPrime(i) == 1))
-			{
-				factor = factor / i;
-				continue;
-			}
-			else
-			{
-				factor = factor;
-			}
-		}
-	}
-	return (factor);
+    return 0;
 }
