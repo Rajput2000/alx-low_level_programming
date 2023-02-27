8074d1eefb98800b219b5d051ce89ef71704e76c
Raji Gbenga Kabir <rajigbenga5@gmail.com>
2023-02-27 05:44:51 -0800

    strcpy

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,19 @@
+#include "main.h"
+#include <string.h>
+
+/**
+ * *_strcpy - is main func
+ * @dest: is tghe var
+ * @src: is the var
+i * Return: success 0
+ */
+char *_strcpy(char *dest, char *src)
+{
+	int i = -1;
+
+	do {
+		i++;
+		dest[i] = src[i];
+	} while (src[i] != '\0');
+	return (dest);
+}
