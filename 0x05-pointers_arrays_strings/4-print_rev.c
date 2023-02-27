9d8d0de296dc5dbfdd76ee5f5fbd774eff879ad4
Raji Gbenga Kabir <rajigbenga5@gmail.com>
2023-02-27 05:20:33 -0800

    I can only go one way. I've not got a reverse gear

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,19 @@
+#include "main.h"
+#include <string.h>
+
+/**
+ * print_rev - is the main function
+ * @s: is the variable
+ * Return: return success 0
+ */
+
+void print_rev(char *s)
+{
+	int a = 0;
+
+	while (s[a] != '\0')
+		a++;
+	while (a)
+		_putchar(s[--a]);
+	_putchar('\n');
+}
