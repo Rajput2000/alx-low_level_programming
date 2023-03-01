a7387ab4d201c336b0438c55dfbe7b8466680065
Raji Gbenga Kabir <rajigbenga5@gmail.com>
2023-02-28 22:40:53 -0800

    strncpy

---
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * _strncpy - a function that copies a string
+ * @dest: is a pointer
+ * @src: is a pointer
+ * @n: is the variable
+ * Return: 0 if is success
+ */
+
+char *_strncpy(char *dest, char *src, int n)
+{
+	int i;
+
+	for (i = 0 ; src[i] != '\0' && i < n ; i++)
+	{
+		dest[i] = src[i];
+	}
+	for ( ; i < n; i++)
+	{
+		dest[i] = '\0';
+	}
+	return (dest);
+}
